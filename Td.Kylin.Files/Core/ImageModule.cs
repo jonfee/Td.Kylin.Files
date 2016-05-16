887814adcba17123ef1f16a6a6dedf767796bb72
伍朝辉 <337883612@qq.com>
2016-05-16 13:41:28 +0800

    转换图片格式

--- Td.Kylin.Files/Core/ImageModule.cs
+++ Td.Kylin.Files/Core/ImageModule.cs
@@ -6,7 +6,11 @@
 // 更新记录:     
 
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Web;
 
@@ -17,9 +21,11 @@ namespace Td.Kylin.Files.Core
     /// </summary>
     public class ImageModule : IHttpModule
     {
+        static string imgExtensions = "jpg|jpeg|gif|bmp|png|ico|pcx|tiff|tga|exif|fpx|svg|psd|cdr|pcd|dxf|ufo|eps|ai|hdri|raw";
+
         public void Dispose()
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
         }
 
         public void Init(HttpApplication appContext)
@@ -28,69 +34,42 @@ namespace Td.Kylin.Files.Core
         }
 
         /// <summary>
-        /// BeginRequest事件发生时的第一个处理
-        /// </summary>
-        /// <param name="application">HttpApplication</param>
-        /// <returns>是否继续执行的表示，True为继续执行，False表示不继续处理后续请求</returns>
-        public virtual bool RequestFirst(HttpApplication application)
-        {
-            return true;
-        }
-
-        /// <summary>
         /// 生成指定规格的图片（如果原图存在的情况）
         /// </summary>
-        /// <param name="application">HttpApplication</param>
-        public virtual void MakeThumbImage(HttpApplication application)
+        /// <param name="physicalPath">访问图片的物理路径</param>
+        public virtual void MakeThumbImage(string physicalPath)
         {
-            //获取请求的物理路径
-            string fileName = application.Request.PhysicalPath;
-
-            if (string.IsNullOrEmpty(fileName)) return;
-
             //文件物理路径大写表示
-            fileName = fileName.ToUpper();
+            physicalPath = physicalPath.ToUpper();
 
-            // 文件已经存在，不做处理
-            if (File.Exists(fileName)) return;
+            var filename = Path.GetFileName(physicalPath);
 
-            //最后一个文件夹分隔字符索引位置（从零开始）
-            var lastSplitIndex = fileName.LastIndexOf("\\");
-
-            //获取文件名称
-            var name = fileName.Substring(lastSplitIndex + 1);
-
-            if (string.IsNullOrEmpty(name)) return; // 为空，不做处理。
-
-            string regStr =
-                string.Format(
-                    @"^{0}W(?<width>\d+)H(?<height>\d+)_([^\.]*\.)+(jpg|jpeg|gif|bmp|png|ico|pcx|tiff|tga|exif|fpx|svg|psd|cdr|pcd|dxf|ufo|eps|ai|hdri|raw)$",
-                    ThumbIdentify.Identify);
-            Regex reg = new Regex(regStr, RegexOptions.IgnoreCase);
-
-            Match m = reg.Match(name);
-
-            if (!m.Success) return; //非特定图片格式，不处理
+            //原图文件替换标识正则
+            Regex originalReg = new Regex(string.Format(@"{0}W\d+H\d+_", ThumbIdentify.Identify), RegexOptions.IgnoreCase);
 
             //原图文件
-            Regex originalReg = new Regex(string.Format(@"{0}W\d+H\d+_", ThumbIdentify.Identify),
-                RegexOptions.IgnoreCase);
-            var originalName = originalReg.Replace(name, "");
+            string originalFileName = originalReg.Replace(physicalPath, "");
 
-            string originalFileName = fileName.Remove(lastSplitIndex + 1) + originalName;
-
-            if (!Directory.Exists(Path.GetDirectoryName(fileName)))
+            if (!Directory.Exists(Path.GetDirectoryName(physicalPath)))
             {
-                Directory.CreateDirectory(Path.GetDirectoryName(fileName)); // 目标目录不存在，创建。
+                Directory.CreateDirectory(Path.GetDirectoryName(physicalPath)); // 目标目录不存在，创建。
             }
 
             try
             {
-                int w = int.Parse(m.Groups["width"].Value);
+                string regStr = string.Format(@"^{0}W(?<width>\d+)H(?<height>\d+)_(?:(?!\.{1}).)+(\.({1}))+$", ThumbIdentify.Identify, imgExtensions);
+                Regex reg = new Regex(regStr, RegexOptions.IgnoreCase);
 
-                int h = int.Parse(m.Groups["height"].Value);
+                Match m = reg.Match(filename);
 
-                ThumbHelper.MakeThumbnailByAutoCut(originalFileName, fileName, w, h);
+                if (null != m)
+                {
+                    int w = int.Parse(m.Groups["width"].Value);
+
+                    int h = int.Parse(m.Groups["height"].Value);
+
+                    ThumbHelper.MakeThumbnailByAutoCut(originalFileName, physicalPath, w, h);
+                }
             }
             catch
             {
@@ -104,21 +83,152 @@ namespace Td.Kylin.Files.Core
             var app = sender as HttpApplication;
 
             //获取请求的物理路径
-            string fileName = app.Request.PhysicalPath;
+            string physicalPath = app.Request.PhysicalPath;
 
-            if (string.IsNullOrEmpty(fileName)) return;
+            var work = GetWork(physicalPath);
 
-            Regex reg = new Regex(@"^([^\.]*\.)+(jpg|jpeg|gif|bmp|png|ico|pcx|tiff|tga|exif|fpx|svg|psd|cdr|pcd|dxf|ufo|eps|ai|hdri|raw)$", RegexOptions.IgnoreCase);
-
-            if (!reg.IsMatch(fileName)) return;//非图片格式，不处理
-
-            //第一步处理
-            var isContinue = RequestFirst(app);
-
-            if (isContinue)
+            switch (work)
             {
-                MakeThumbImage(app);
+                case WorkType.TransferFormat:
+                    TransferFormat(physicalPath);
+                    break;
+                case WorkType.MakeThumbImage:
+                    MakeThumbImage(physicalPath);
+                    break;
             }
+        }
+
+        private void TransferFormat(string physicalPath)
+        {
+            //原图文件
+            string oldFile = physicalPath.Remove(physicalPath.LastIndexOf('.'));
+
+            //扩展名
+            string extension = Path.GetExtension(physicalPath);
+
+            Image image = Image.FromFile(oldFile);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                ImageFormat format = ImageFormat.Jpeg;
+                ImageCodecInfo codecInfo = ImageCodecInfo.GetImageEncoders().Where(p => p.FilenameExtension.Contains(extension.ToUpper())).FirstOrDefault();
+                if (codecInfo != null)
+                {
+                    PropertyInfo prop = typeof(ImageFormat).GetProperties().Where(p => p.Name.Equals(codecInfo.FormatDescription, StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+                    if (prop != null)
+                    {
+                        format = prop.GetValue(prop, null) as ImageFormat;
+                    }
+                }
+
+                image.Save(ms, format);
+
+                Bitmap bitmap = new Bitmap(ms);
+
+                bitmap.Save(physicalPath);
+            }
+        }
+
+        private WorkType GetWork(string physicalPath)
+        {
+            if (string.IsNullOrWhiteSpace(physicalPath)) return WorkType.NoAction;
+
+            //文件已经存在，不做处理
+            if (File.Exists(physicalPath)) return WorkType.NoAction;
+
+            //获取文件名称
+            var filename = Path.GetFileName(physicalPath);
+
+            if (string.IsNullOrEmpty(filename)) return WorkType.NoAction;
+
+            //检测是否为图片
+            bool isImage = IsImage(filename);
+
+            //非图片格式，不处理
+            if (!isImage) return WorkType.NoAction;
+
+            //是否为转换格式
+            bool isTransType = false;
+            Regex regTrans = new Regex(string.Format(@"(\.({0})){1}$", imgExtensions, "{2,}"), RegexOptions.IgnoreCase);
+            if (regTrans.IsMatch(filename))
+            {
+                string beforeTransferFile = GetBeforeTransferFilePath(physicalPath);
+                if (File.Exists(beforeTransferFile)) isTransType = true;
+            }
+
+            //转换格式操作
+            if (isTransType) return WorkType.TransferFormat;
+
+            string regStr = string.Format(@"^{0}W\d+H\d+_(?:(?!\.{1}).)+(\.({1}))+$", ThumbIdentify.Identify, imgExtensions);
+            Regex reg = new Regex(regStr, RegexOptions.IgnoreCase);
+            if (reg.IsMatch(filename))
+            {
+                return WorkType.MakeThumbImage;
+            }
+
+            return WorkType.NoAction;
+        }
+
+        /// <summary>
+        /// 是否为图片
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        private bool IsImage(string filename)
+        {
+            if (string.IsNullOrWhiteSpace(filename)) return false;
+
+            Regex reg = new Regex(string.Format(@"\.({0})$", imgExtensions), RegexOptions.IgnoreCase);
+
+            return reg.IsMatch(filename);
+        }
+
+        /// <summary>
+        /// 获取扩展名
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        private string GetExtension(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath)) return string.Empty;
+
+            return Path.GetExtension(filePath);
+        }
+
+        /// <summary>
+        /// 获取转换前的原文件
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        private string GetBeforeTransferFilePath(string filename)
+        {
+            Regex reg = new Regex(string.Format(@"(\.({0})){1}$", imgExtensions, "{2,}"), RegexOptions.IgnoreCase);
+
+            if (reg.IsMatch(filename))
+            {
+                return filename.Remove(filename.LastIndexOf('.'));
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 工作类型
+        /// </summary>
+        enum WorkType
+        {
+            /// <summary>
+            /// 无操作
+            /// </summary>
+            NoAction,
+            /// <summary>
+            /// 生成缩略图
+            /// </summary>
+            MakeThumbImage,
+            /// <summary>
+            /// 转换格式
+            /// </summary>
+            TransferFormat
         }
     }
 }
